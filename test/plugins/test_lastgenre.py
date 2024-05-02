e34a22690e9891af4f51f7d31718c8ad1eb2ca82
ubuntu <ubuntu@ip-10-0-0-248>
2024-05-02 11:04:21 +0000

    test_lca_code_editing__code-editing-metrics-xead0287a17953ac__id_38

--- test/plugins/test_lastgenre.py
+++ test/plugins/test_lastgenre.py
@@ -20,15 +20,16 @@ from test import _common
 from test.helper import TestHelper
 from unittest.mock import Mock
 
+from beets.testing import unittest
+from beets.testing import TestHelper
 from beets import config
 from beetsplug import lastgenre
 
-
 class LastGenrePluginTest(unittest.TestCase, TestHelper):
     def setUp(self):
+        super().setUp()
         self.setup_beets()
         self.plugin = lastgenre.LastGenrePlugin()
-
     def tearDown(self):
         self.teardown_beets()
 
