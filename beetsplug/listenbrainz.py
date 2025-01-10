c265e5b08c496d02d70ea3c7cfc1fbd5c691e712
ubuntu <ubuntu@ip-10-0-1-21>
2025-01-10 12:22:01 +0000

    waleko__claude-3.5-sonnet_d33e__id_43

--- beetsplug/listenbrainz.py
+++ beetsplug/listenbrainz.py
@@ -150,7 +150,9 @@ class ListenBrainzPlugin(BeetsPlugin):
     def get_listenbrainz_playlists(self):
         """Returns a list of playlists created by ListenBrainz."""
         import re
+
         resp = self.get_playlists_createdfor(self.username)
+
         playlists = resp.get("playlists")
         listenbrainz_playlists = []
 
@@ -158,7 +160,9 @@ class ListenBrainzPlugin(BeetsPlugin):
             playlist_info = playlist.get("playlist")
             if playlist_info.get("creator") == "listenbrainz":
                 title = playlist_info.get("title")
-                match = re.search(r"(Missed Recordings of \d{4}|Discoveries of \d{4})", title)
+                match = re.search(
+                    r"(Missed Recordings of \d{4}|Discoveries of \d{4})", title
+                )
                 if "Exploration" in title:
                     playlist_type = "Exploration"
                 elif "Jams" in title:
@@ -169,9 +173,7 @@ class ListenBrainzPlugin(BeetsPlugin):
                     playlist_type = None
                 if "week of " in title:
                     date_str = title.split("week of ")[1].split(" ")[0]
-                    date = datetime.datetime.strptime(
-                        date_str, "%Y-%m-%d"
-                    ).date()
+                    date = datetime.datetime.strptime(date_str, "%Y-%m-%d").date()
                 else:
                     date = None
                 identifier = playlist_info.get("identifier")
