abbfe0bf1e1fb1cfe63e0dbbb83bef71fd651a5b
ubuntu <ubuntu@ip-10-0-1-33>
2025-01-15 16:39:03 +0000

    waleko__claude-3.5-sonnet_d261__id_38

--- beetsplug/edit.py
+++ beetsplug/edit.py
@@ -23,10 +23,10 @@ from tempfile import NamedTemporaryFile
 
 import yaml
 
-from beets import plugins, ui, util, config
+from beets import config, plugins, ui, util
 from beets.dbcore import types
 from beets.importer import action
-from beets.ui.commands import PromptChoice, _do_query
+from beets.ui.commands import _do_query, PromptChoice
 
 # These "safe" types can avoid the format/parse cycle that most fields go
 # through: they are safe to edit with native YAML types.
